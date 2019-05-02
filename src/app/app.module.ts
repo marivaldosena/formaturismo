caded80f2e876dde41b2a1cf58f5ba9b5c85aa91
TI Forma Turismo <formati@formati.com>
2019-05-02 16:26:48 -0300

    Criar página de confirmação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ConfirmacaoComponent } from './confirmacao/confirmacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    ConfirmacaoComponent
   ],
   imports: [
     FormsModule,
